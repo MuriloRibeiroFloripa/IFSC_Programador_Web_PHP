bd5af6b8d1236b451f98a0ab6a4a2cfe740f9ae9
Murilosenai <murilo_ribeiro1@estudante.sc.senai.br>
2021-06-08 19:15:34 -0300

    Fundamentos de Programação - Variáveis

--- index.php
+++ index.php
@@ -3,4 +3,12 @@
 echo "<h1>Funcionando </h1>";
 echo "<h2>Funcionando bem</h2>";
 
+$nomealuno="Murilo Ribeiro";
+echo $nomealuno;
+
+echo "<br>";
+
+$nomealuno1="Tatiane da luz Silva";
+echo $nomealuno1;
+
 ?>
