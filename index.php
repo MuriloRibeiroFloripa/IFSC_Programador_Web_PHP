04fc9ce553333b6dca9ed16e18fa4df6217db388
Murilosenai <murilo_ribeiro1@estudante.sc.senai.br>
2021-06-08 20:13:31 -0300

    Fundamentos de Programação - Atribuição de Variáveis por GET - Exibição em tabela

--- index.php
+++ index.php
@@ -1,28 +1,57 @@
-<?php 
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>PHP com HTML</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<?php 
+	echo "<h1>Funcionando </h1>";
+	echo "<h2>Funcionando bem</h2>";
 
-echo "<h1>Funcionando </h1>";
-echo "<h2>Funcionando bem</h2>";
+	$nomealuno = "Murilo Ribeiro";
+	$idadealuno = "35";
+	echo "Nome do Aluno = ";
+	echo $nomealuno;
+	echo " Idade = ";
+	echo $idadealuno;
 
-$nomealuno = "Murilo Ribeiro";
-$idadealuno = "35";
-echo "Nome do Aluno = ";
-echo $nomealuno;
-echo " Idade = ";
-echo $idadealuno;
-echo "<br>";
+	echo "<br>";
 
-$nomealuno1="Tatiane da luz Silva";
-$idadealuno1 = "33";
-echo "Nome da Aluna = ";
-echo $nomealuno1;
-echo " Idade = ";
-echo $idadealuno1;
-echo "<br>";
+	$nomealuno1="Tatiane da luz Silva";
+	$idadealuno1 = "33";
+	echo "Nome da Aluna = ";
+	echo $nomealuno1;
+	echo " Idade = ";
+	echo $idadealuno1;
 
-//Fundamentos de Programação - Atribuição de Variáveis por GET
-// localhost/IFSC_Programador_Web_PHP/index.php?nome=Nome%20do%20Aluno%20=%20Murilo%20Silva
-//echo $_GET['nome'];
-$nomealuno2 = $_GET['nome'];
-echo $nomealuno2;
+	echo "<br>";
 
-?>
+	//Fundamentos de Programação - Atribuição de Variáveis por GET
+	// localhost/IFSC_Programador_Web_PHP/index.php?nome=Marcos Silva
+	$nomealuno2 = $_GET['nome'];
+	echo "Nome do Aluno = ";
+	echo $nomealuno2;
+	// localhost/IFSC_Programador_Web_PHP/index.php?nome=Marcos%20Silva&idade=30
+	$idadealuno2 = $_GET['idade'];
+	echo " Idade = ";
+	echo $idadealuno2;
+	echo "<br>";
+	echo "<br>";
+	?>
+
+	<table border="1">
+		<tr>
+			<td>Nome do Aluno = <?php echo $nomealuno; ?><td>Idade = <?php echo $idadealuno ?></td>
+		</tr>
+		<tr>
+			<td>Nome da Aluna = <?php echo $nomealuno1; ?><td>Idade = <?php echo $idadealuno1 ?></td>
+		</tr>
+		<tr>
+			<td>Nome do Aluno = <?php echo $nomealuno2; ?><td>Idade = <?php echo $idadealuno2 ?></td>
+		</tr>
+	</table>	
+</body>
+</html>
